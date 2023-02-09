43f6d77553f549604817621e5b70599a2076cfe7
creedvi <cdayvi@gmail.com>
2023-02-10 05:30:02 +0900

    work

---
+++ script/app_main.js
@@ -0,0 +1,35 @@
+var app = angular.module('StudioVI_View');
+
+app.config(['$locationProvider', function($locationProvider) {
+    $locationProvider.html5Mode(false).hashPrefix('');
+}]);
+
+app.config(['$routeProvider', function($routeProvider) {
+
+    $routeProvider
+    .when("/home", {
+        templateUrl : "views/holder.html",
+        controller  : "home-controller"
+    })
+    .when("/contact", {
+        templateUrl : "views/contact.php",
+        controller  : "contact-controller"
+    })
+    .when("/notFound", {
+        templateUrl : "views/notFound.html",
+        controller  : ""
+    })
+    .otherwise({
+        redirectTo : "/home"
+    });
+
+}]);
+
+app.controller('contact-controller',['$scope', function($scope){
+}]);
+
+app.controller('confirm-controller',['$scope', function($scope){
+
+    setTimeout("location.href = 'http://studiovi.io';",500000);
+
+}]);
